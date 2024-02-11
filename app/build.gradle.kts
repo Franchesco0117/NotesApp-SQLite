ff6a6793a71a70ad1d7606071a58b93a539eeb7b
franciscocastromurillo18@gmail.com <franciscocastromurillo18@gmail.com>
2024-02-11 14:24:51 -0700

    SQLite Logic - Creation of the DataBase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.mynotessqlite"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.mynotessqlite"
---
+++ app/src/main/java/com/example/mynotessqlite/Note.kt
@@ -0,0 +1,3 @@
+package com.example.mynotessqlite
+
+data class Note(val id: Int, val title: String, val content: String)
